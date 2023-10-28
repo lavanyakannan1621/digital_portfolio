4025c80a50a7a98567506aa87485da4241871872
lavanyakannan1621 <149140211+lavanyakannan1621@users.noreply.github.com>
2023-10-28 15:18:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Sample data for projects
+const projects = [
+    { title: "Project 1", description: "Description 1", url: "https://project1.com" },
+    { title: "Project 2", description: "Description 2", url: "https://project2.com" },
+    // Add more project objects
+];
+
+
