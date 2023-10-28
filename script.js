fafc011afa6242f7a8d9b02070b5034a1e96fef6
lavanyakannan1621 <149140211+lavanyakannan1621@users.noreply.github.com>
2023-10-28 18:27:32 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
-// Sample data for projects
+// Sample project data
 const projects = [
-    { title: "Project 1", description: "Description 1", url: "https://project1.com" },
-    { title: "Project 2", description: "Description 2", url: "https://project2.com" },
-    // Add more project objects
+    {
+        title: "Project 1",
+        description: "Description of project 1.",
+        link: "https://project1link.com",
+    },
+    {
+        title: "Project 2",
+        description: "Description of project 2.",
+        link: "https://project2link.com",
+    },
+    // Add more projects here
 ];
+
+// Function to populate the projects section
+function populateProjects() {
+    const projectList = document.getElementById("project-list");
+
+    projects.forEach((project) => {
+        const projectDiv = document.createElement("div");
+        projectDiv.innerHTML = `
+            <h3>${project.title}</h3>
+            <p>${project.description}</p>
+            <a href="${project.link}" target="_blank">View Project</a>
+        `;
+        projectList.appendChild(projectDiv);
+    });
+}
+
+// Call the function to populate the projects
+populateProjects();
+
 
 
